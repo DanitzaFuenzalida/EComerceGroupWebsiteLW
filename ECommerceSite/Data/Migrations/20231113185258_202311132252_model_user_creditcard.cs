5a2ccf982f4e06fdc60ff8ca88d7b03db473dd00
Kavita Shokeen <kavitashokeen@gmail.com>
2023-11-13 10:54:19 -0800

    Extending User Model to include credit card number, age, interests https://group4-csd.atlassian.net/browse/TEAM-29

---
+++ ECommerceSite/Data/Migrations/20231113185258_202311132252_model_user_creditcard.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceSite.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _202311132252_model_user_creditcard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreditCardNumber",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Interests",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "CreditCardNumber",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Interests",
+                table: "User");
+        }
+    }
+}
--- ECommerceSite/Model/User.cs
+++ ECommerceSite/Model/User.cs
@@ -3,10 +3,15 @@ namespace ECommerceSite.Model
     public class User
     {
         public int UserId { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; } // You should hash and salt the password.
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; } // You should hash and salt the password.
                                              // Add other user-related properties like addresses, roles, etc.
+        public string? CreditCardNumber {  get; set; }
+
+        public int Age { get; set; }
+        public string? Interests { get; set; }
+
     }
 
 }
